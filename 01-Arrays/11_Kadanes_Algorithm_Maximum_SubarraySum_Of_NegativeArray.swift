dd074bdd7d5b8c3da53617d4bb62efb465ee6449
Sanjay Morya <skmorya05@gmail.com>
2026-08-25 16:40:36 +0800

    Maximum Subarray (Kadanes Algoritham) when all array elements will be negative.

---
+++ 01-Arrays/11_Kadanes_Algorithm_Maximum_SubarraySum_Of_NegativeArray.swift
@@ -0,0 +1,41 @@
+/*
+Problem: Maximum Subarray (Kadanes Algoritham)
+Difficulty: Medium
+Pattern: Kadanes Algoritham
+Leetcode: https://leetcode.com/problems/maximum-subarray/description/
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+#Example 1:
+Input: nums = [-2,-3,-1,-5,-8,-4]
+Output: -1
+Explanation: The subarray [-1] has the largest sum -1.
+
+#Example 2:
+Input: nums = [-1,-3,-3,-2,-1]
+Output: -1
+Explanation: The subarray [-1] has the largest sum -1.
+*/
+
+func maxSubArray(_ nums: [Int]) -> Int {
+    guard let first = nums.first else {
+        return 0
+    }
+    var currentSum = first
+    var maxSum = first
+    for i in 1..<nums.count {
+        currentSum = max(nums[i], currentSum + nums[i])
+        maxSum = max(maxSum, currentSum)
+    }
+    return maxSum
+}
+
+let result1 = maxSubArray([-2,-3,-1,-5,-8,-4])
+print("result1 = \(result1) expected is -1")
+
+let result2 = maxSubArray([-1,-3,-3,-2,-1])
+print("result2 = \(result2) expected is -1")
+
